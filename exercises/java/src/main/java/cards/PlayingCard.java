d94daca5fd54756c176b0f4c010d5c005cd9425d
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2025-03-11 12:23:21 +0000

    Create PlayingCard Class and use it in Cards

---
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -0,0 +1,12 @@
+package cards;
+
+public class PlayingCard {
+
+    int suit;
+    int faceValue;
+
+    public PlayingCard (int faceValue, int suit){
+        this.faceValue = faceValue;
+        this.suit = suit;
+    }
+}
