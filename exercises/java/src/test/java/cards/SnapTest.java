8ae3e58d06456b864d3f2df93dfffb961ceeb489
Jamie Gernon <jamie.gernon@autotrader.co.uk>
2025-03-12 10:59:50 +0000

    Add tests for Snap

--- exercises/java/src/test/java/cards/SnapTest.java
+++ exercises/java/src/test/java/cards/SnapTest.java
@@ -1,7 +1,13 @@
 package cards;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class SnapTest extends TestCase {
+
+    @Test
+    public void whenSnapIsCalledCorrectlyAddsOnePoint() {
+
+    }
 
 }
